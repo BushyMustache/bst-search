2c48ae7e297a2d91a221306b895a5ec69dfe0240
BushyMustache <205280148+BushyMustache@users.noreply.github.com>
2026-09-22 17:09:49 -0700

    Written the method and tested if exception thrown correctly

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    @Test
+    void containsEmptyTree() {
+        BinaryTreeNode<Integer> root = null;
 
+        assertFalse(BstSearch.contains(root, 5));
+    }
 }
